0f283a7c23703ca5e7905dcaee25a13a825ca3e5
dan <dan@thewisemans.io>
2024-06-30 07:41:38 -0400

    refactor: Replace redis with ioredis in cache implementation and dependencies.

--- api/_cache.ts
+++ api/_cache.ts
@@ -1,12 +1,11 @@
-import { createClient } from "redis";
+import { Redis } from "ioredis";
 
-export const cacheClient = createClient({
+export const cacheClient = new Redis({
+    port: parseInt(process.env.REDISPORT || '6379', 10), // Redis port
+    host: process.env.REDISHOST, // Redis host
     password: process.env.REDIS_PASSWORD,
-    socket: {
-        host: process.env.REDISHOST,
-        port: parseInt(process.env.REDISPORT || '6379', 10)
-    }
 });
+
 
 export async function checkCachedEventId(eventID : string) {
 
