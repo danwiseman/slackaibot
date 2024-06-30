760644954cfe263e2a568175f0bf83a17beb1cd5
dan <dan@thewisemans.io>
2024-06-30 07:28:44 -0400

    fixed port to match railway vars

--- api/_cache.ts
+++ api/_cache.ts
@@ -4,7 +4,7 @@ export const cacheClient = createClient({
     password: process.env.REDIS_PASSWORD,
     socket: {
         host: process.env.REDISHOST,
-        port: parseInt(process.env.REDIS_PORT || '6379', 10)
+        port: parseInt(process.env.REDISPORT || '6379', 10)
     }
 });
 
